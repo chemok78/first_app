7ab8567441edd68ce4c7fcdc3c64dbc9c36ea4b4
Jay Mok <chemok78@gmail.com>
2014-09-23 00:42:14 +0800

    rails installation, added Gems and tweaked gitignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '34a1451917399181eeb1fd8a20683e640027676800e3d046b4bb243afd671d3822c47af2710c70e04118af7120289363c37f41464b61039cda7cd89d6f5827d7'
